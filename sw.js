b436bdb09ce1d970468b55bddd450dbe5d28bac2
itsvalvs <vnuzsaro@gmail.com>
2026-06-15 21:05:26 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
 //  reinstala solo y limpia las cachés antiguas.
 // ═══════════════════════════════════════════════════════════
 
-const VERSION = 'ylp-v14';
+const VERSION = 'ylp-v15';
 
 const PRECACHE = [
   './',
